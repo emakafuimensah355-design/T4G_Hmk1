93dec76bf29621786181aecff96163b37b20fa30
emakafuimensah355-design <emakafuimensah355@gmail.com>
2026-07-20 13:16:09 +0400

    adding all new changes on index.html and script.js

---
+++ src/script.js
@@ -0,0 +1,7 @@
+// Prompt user for information
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email address:");
+
+// Display the information
+alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
