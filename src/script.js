556a528ae18ff5327b0ec6a749d7aa6117398c14
emakafuimensah355-design <emakafuimensah355@gmail.com>
2026-07-23 12:11:51 +0400

    adding all changes

--- src/script.js
+++ src/script.js
@@ -5,3 +5,43 @@ let email = prompt("Enter your email address:");
 
 // Display the information
 alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
+
+// A. Store your name inside a variable and log it to the console
+let myName = "Emmanuella Makafui Mensah";
+console.log("Name:", myName);
+
+// B. Store your age inside a variable and log it to the console
+let myAge = 20;
+console.log("Age:", myAge);
+
+// C. Add two numbers and store it in a variable and log it to the console
+let addition = 10 + 5;
+console.log("Addition:", addition);
+
+// D. Subtract two numbers and store it in a variable and log it to the console
+let subtraction = 10 - 5;
+console.log("Subtraction:", subtraction);
+
+// E. Divide two numbers and store it in a variable and log it to the console
+let division = 10 / 5;
+console.log("Division:", division);
+
+// F. Multiply two numbers and store it in a variable and log it to the console
+let multiplication = 10 * 5;
+console.log("Multiplication:", multiplication);
+
+// G. Find the remainder of 15 % 4 and store it in a variable and log it to the console
+let remainder = 15 % 4;
+console.log("Remainder:", remainder);
+
+// H. Store a boolean value inside a variable and log it to the console
+let isStudent = true;
+console.log("Boolean Value:", isStudent);
+
+// I. Set the value of a variable to a number and log the type of data it is
+let numberValue = 100;
+console.log("Type of numberValue:", typeof numberValue);
+
+// J. Set the value of a variable to a string and log the type of data it is
+let stringValue = "JavaScript";
+console.log("Type of stringValue:", typeof stringValue);
